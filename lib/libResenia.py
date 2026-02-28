acfd0d19163e25309cb76b267142cc0e669ef5d0
SsamirNunez360 <ssamirnunez147@gmail.com>
2026-02-28 07:05:55 -0600

    Cambios en libResenia.py

--- lib/libResenia.py
+++ lib/libResenia.py
@@ -3,56 +3,70 @@ import json
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FILE_PATH = os.path.join(BASE_DIR, "data/resenias.json")
+
 
 class LibResenia:
-    
-    def create(self,id, descripcion):
+
+    def create(self, id, descripcion):
         resenia = Resenia(id, descripcion)
-        file = open(os.path.join(BASE_DIR, "data/resenias.json"))
-        j = json.load(file)
-        j = j + [resenia.toJson()]
-        file = open(os.path.join(BASE_DIR, "data/resenias.json"),"w")
-        json.dump(j,file,ensure_ascii=False, indent=4)
-        file.close()
+
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            j = json.load(file)
+
+        j.append(resenia.toJson())
+
+        with open(FILE_PATH, "w", encoding="utf-8") as file:
+            json.dump(j, file, ensure_ascii=False, indent=4)
+
         return True
-    
+
     def get_resenia(self):
-        file = open(os.path.join(BASE_DIR, "data/resenias.json"))
-        j = json.load(file)
-        list = []
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            j = json.load(file)
+
+        lista = []
         for x in j:
-            list.append(Resenia(x['id'],x['descripcion']))
-        return list 
-    
+            lista.append(Resenia(x['id'], x['descripcion']))
+
+        return lista
+
     def get_resenia_by_id(self, id):
-        file = open(os.path.join(BASE_DIR, "data/resenias.json"))
-        j = json.load(file)
-        for x in j: 
-            if(x['id']==id):
-                return Resenia(x['id'],x['descripcion'])
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            j = json.load(file)
+
+        for x in j:
+            if x['id'] == id:
+                return Resenia(x['id'], x['descripcion'])
+
         return None
-    
-    def edit_resenia(self,id, descripcion):
-        file = open(os.path.join(BASE_DIR, "data/resenias.json"))
-        j = json.load(file)
-        for x in j: 
-            if(x['id']==id):
+
+    def edit_resenia(self, id, descripcion):
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            j = json.load(file)
+
+        for x in j:
+            if x['id'] == id:
                 x['descripcion'] = descripcion
-                file = open(os.path.join(BASE_DIR, "data/resenias.json"),"w")
-                json.dump(j,file,ensure_ascii=False, indent=4)
-                file.close()
+
+                with open(FILE_PATH, "w", encoding="utf-8") as file:
+                    json.dump(j, file, ensure_ascii=False, indent=4)
+
                 return True
+
         return False
-    
+
     def eliminar(self, id):
-        file = open(os.path.join(BASE_DIR, "data/resenias.json"))
-        j = json.load(file)
-        for i in range(0,len(j)): 
-            if(j[i]['id']==id):
-                j = j[:i]+ j[i+1:]
-                file = open(os.path.join(BASE_DIR, "data/resenias.json"),"w")
-                json.dump(j,file,ensure_ascii=False, indent=4)
-                file.close()
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            j = json.load(file)
+
+        for i in range(len(j)):
+            if j[i]['id'] == id:
+                j.pop(i)
+
+                with open(FILE_PATH, "w", encoding="utf-8") as file:
+                    json.dump(j, file, ensure_ascii=False, indent=4)
+
                 return True
+
         return False
-    
